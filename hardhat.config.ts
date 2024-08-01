16a5adea810a51d0a5f0410027aa64a3ace31425
sakshammaggu <sakshammaggu80@gmail.com>
2024-08-01 19:43:27 +0530

    test and deploy helloWorld smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    localganache: {
+      url: process.env.PROVIDER_URL,
+      accounts: [process.env.PRIVATE_KEY ?? '']
+    }
+  }
 };
 
 export default config;
